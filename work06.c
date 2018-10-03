9fcee67e2e2268c0e9a47f90ea23a9785b65072e
Rubin Peci <rubinpeci@gmail.com>
2018-10-03 11:19:16 -0400

    Made some fixes

--- work06.c
+++ work06.c
@@ -1,21 +1,27 @@
 #include <strings.h>
 #include <stdio.h>
-struct Breakfast{char *item; char *item2;};
 
-struct Breakfast create(){
-  struct Breakfast menu = {"eggs", "bacon"};
+struct Breakfast{ char *item; char *item2; };
+
+struct Breakfast create()
+{
+ 	struct Breakfast menu = {"eggs", "bacon"};
+	return menu;
 }
 
-void prints(struct Breakfast menu){
-  printf("%s , %s \n", menu -> item , menu -> item2);
+void prints(struct Breakfast menu)
+{
+	printf("%s , %s \n", menu.item , menu.item2);
 }
 
-void change(struct Breakfast menu, char* new){
+void change(struct Breakfast menu, char* new)
+{
 }
 
-int main(){
-  struct Breakfast test = create();
-  printf("%s\n", test.item);
-  return 0;
+int main()
+{
+	struct Breakfast test = create();
+	prints(test);
+	return 0;
 }
   
