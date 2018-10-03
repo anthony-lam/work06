b26726f5661d6bdda65d1aff33648a151a816a3c
Rubin Peci <rubinpeci@gmail.com>
2018-10-03 11:27:49 -0400

    Modify what the menu stores

--- struct.c
+++ struct.c
@@ -1,17 +1,17 @@
 #include <strings.h>
 #include <stdio.h>
 
-struct Breakfast{ char *item; char *item2; };
+struct Breakfast{ char *item; double  price; int calories; };
 
 struct Breakfast create()
 {
- 	struct Breakfast menu = {"eggs", "bacon"};
+ 	struct Breakfast menu = {"eggs", 1.99, 300};
 	return menu;
 }
 
 void prints(struct Breakfast menu)
 {
-	printf("%s , %s \n", menu.item , menu.item2);
+	printf("Item: %s\nPrice: %.2f\nCalories: %d\n", menu.item , menu.price, menu.calories);
 }
 
 void change(struct Breakfast menu, char* new)
