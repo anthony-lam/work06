6cda2ef1254a851ff73d98e777310233fda29efd
AnthonyLam <alam6@stuy.edu>
2018-10-03 21:59:23 -0400

    added more options and made output look better

--- struct.c
+++ struct.c
@@ -14,19 +14,25 @@ struct Breakfast{ char *item; double  price; int calories; };
 //the create method returns a breakfast based off a randomly generated number
 struct Breakfast create()
 {
-	srand(time(NULL));
 	struct Breakfast menu;
-	int num = rand() % 10;
-	if (num < 4){
+	int num = rand() % 6;
+	if (num == 0){
 		struct Breakfast menu = {"eggs", 1.99, 300};
 	}
-	else{
-		if (num < 7){
-		  struct Breakfast menu = {"bacon", 2.99, 600};
-		}
-		else{
-		  struct Breakfast menu = {"pancakes", 5.99, 900};
-		}
+	if (num == 1){
+		struct Breakfast menu = {"bacon", 2.99, 600};
+	}
+	if(num==2){
+		struct Breakfast menu = {"pancakes", 5.99, 900};
+	}
+	if(num==3){
+		struct Breakfast menu = {"toast", 3.99, 800};
+	}
+	if(num==4){
+		struct Breakfast menu = {"sausage", 4.99, 700};
+	}
+	if(num==5){
+		struct Breakfast menu = {"bagel", 4.49, 600};
 	}
 	return menu;
 }
@@ -46,11 +52,20 @@ void change(struct Breakfast *menu, char *_item, double _price, int _calories)
 }
 
 int main()
-{
-	struct Breakfast test = create();
-	prints(test);
-	change(&test, "cereal", 0.99, 200);
-	prints(test);
+{	
+	srand(time(NULL));
+	printf("====Creates a new Breakfast Item====\n\n");
+	struct Breakfast first = create();
+	prints(first);
+	printf("\n====Creates a new Breakfast Item====\n\n");
+	struct Breakfast second = create();
+	prints(second);
+	printf("\n====Creates a new Breakfast Item====\n\n");
+	struct Breakfast third = create();
+	prints(third);
+	printf("\n====Changes breakfast to waffles====\n\n");
+	change(&third, "waffles", 6.99, 1000);
+	prints(third);
 
 	return 0;
 }
